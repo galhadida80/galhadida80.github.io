504b222af5d34530e5a29c370efdaa6ef9544780
גל חדידה <hadidd.gal@msmail.ariel.ac.il>
2021-01-25 00:07:41 +0200

    Updates

---
+++ precache-manifest.ed5912266fc8626eacf5b4bf29430c71.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da83299628a14c79d478d65ef9a2aec",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "f56c63b4d5fb12714e40",
+    "url": "/home/static/css/main.a1220f6c.chunk.css"
+  },
+  {
+    "revision": "dda42dc312b8d1905405",
+    "url": "/home/static/js/2.e5184c73.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.e5184c73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f56c63b4d5fb12714e40",
+    "url": "/home/static/js/main.e07baefe.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "e564a40797c528b934e0408a747a0977",
+    "url": "/home/static/media/gal-hadida.e564a407.png"
+  },
+  {
+    "revision": "2980bbd42c6327b45eac958ff59b5c2a",
+    "url": "/home/static/media/resume.2980bbd4.pdf"
+  }
+]);
